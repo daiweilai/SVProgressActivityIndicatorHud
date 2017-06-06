706f398828bd937fd8aa44a290d48ef353e8dfe4
daiweilai <daiweilai@foxmail.com>
2017-06-06 16:15:51 +0800

    init project

---
+++ SVProgressActivityIndicatorHud/SVProgressActivityIndicatorHud/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SVProgressActivityIndicatorHud
+//
+//  Created by DavidDay on 2017/6/6.
+//  Copyright © 2017年 DavidDay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SVProgressActivityIndicatorHud/SVProgressActivityIndicatorHud/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SVProgressActivityIndicatorHud
+//
+//  Created by DavidDay on 2017/6/6.
+//  Copyright © 2017年 DavidDay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
